58551150eefd308da4667b6c011e6a3f0f837b34
mleiman0822 <matthewtleiman@gmail.com>
2023-01-09 16:43:09 -0700

    init commit

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <img src='' alt=''/>
+        <h1>This is the nav</h1>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQJi27UPuw10NgF3MP61poJnMY9XEUdLI",
+  authDomain: "netflix-clone-3406b.firebaseapp.com",
+  projectId: "netflix-clone-3406b",
+  storageBucket: "netflix-clone-3406b.appspot.com",
+  messagingSenderId: "90521011156",
+  appId: "1:90521011156:web:6e8844f72dd8f91e160c41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
