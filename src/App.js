e9d1457a770df17f94af4f922a657cd62cfbaa00
mleiman0822 <matthewtleiman@gmail.com>
2023-01-10 10:38:33 -0700

    login/sign in screen plus firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import HomeScreen from './HomeScreen';
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-        <HomeScreen/>
+      <Router>
+        {!user ?(
+          <LoginScreen/>
+        ) : (
+          <Routes>
+          <Route path="/" element={<HomeScreen />}/>
+        </Routes>
+        )}
+
+      </Router>
     </div>
   );
 }
