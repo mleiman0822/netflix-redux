cdf1cbdc4e43caf69198fab3ac94e76fd59e8236
mleiman0822 <matthewtleiman@gmail.com>
2023-01-10 11:28:05 -0700

    sign in and sign out functionality completed and working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import {useEffect, React} from "react";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import { auth } from "./firebase";
 import "./App.css";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth){
+        //loggin in 
+        console.log(userAuth)
+      }else{
+        //logged out
+      }
+    });
+    //if component unmounts, we dont want to dupliacte the listener
+    //when it cleans up, run unsubscribe function
+    return unsubscribe;
+  }, [])
+  
 
   return (
     <div className="app">
