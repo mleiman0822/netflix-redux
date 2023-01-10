adf0b74be622d6d832f01fc353e0e858470617eb
mleiman0822 <matthewtleiman@gmail.com>
2023-01-10 13:35:41 -0700

    stripe, redux stuff, boilerplate code removal

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import {useEffect, React} from "react";
+import { useEffect, React } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { auth } from "./firebase";
+import { logout, login, selectUser } from "./features/userSlice";
 import "./App.css";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      if(userAuth){
-        //loggin in 
-        console.log(userAuth)
-      }else{
-        //logged out
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //pass in payload to login method in redux
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        //when logout method in redux is dispatched, user is logged out
+        dispatch(logout());
       }
     });
     //if component unmounts, we dont want to dupliacte the listener
     //when it cleans up, run unsubscribe function
     return unsubscribe;
-  }, [])
-  
+  }, []);
 
   return (
     <div className="app">
       <Router>
-        {!user ?(
-          <LoginScreen/>
+        {!user ? (
+          <LoginScreen />
         ) : (
           <Routes>
-          <Route path="/" element={<HomeScreen />}/>
-        </Routes>
+            <Route path="/profile" element={<ProfileScreen/>}/>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
         )}
-
+        {console.log(user)}
       </Router>
     </div>
   );
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { selectUser } from '../features/userSlice';
+import { useSelector } from 'react-redux';
+import Nav from '../Nav'
+import './ProfileScreen.css'
+import { auth } from '../firebase';
+
+function ProfileScreen() {
+  const user = useSelector(selectUser);
+  return (
+    <div className='profileScreen'>
+        <Nav/>
+        <div className='profileScreen__body'>
+            <h1>Edit Profile</h1>
+            <div className='profileScreen__info'>
+                <img
+                src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+                alt=''
+                />
+                <div className='profileScreen_details'>
+                    <h2>{user.email}</h2>
+                    <div className='profileScreen__plans'>
+                        <h3>Plans</h3>
+                        <button
+                         onClick={()=> auth.signOut()}
+                         className='profileScreen_signOut'>
+                            Sign Out
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProfileScreen
