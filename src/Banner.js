7380d50c0e31ccf0321cb3eefde2be763906406c
mleiman0822 <matthewtleiman@gmail.com>
2023-01-09 18:04:52 -0700

    small banner updates

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './Banner.css'
 
 function Banner() {
   return (
-    <div className='banner'>
-
-    </div>
+    <header className='banner' style={{
+        backgroundSize:"cover",
+        backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
+        backgroundPosition:"center center"
+    }}>
+        <div className='banner__contents'>
+            <h1 className='banner__title'>Movie Name</h1>
+            <div className='banner__buttons'>
+                <button>Play</button>
+                <button>My List</button>
+            </div>
+        </div>
+    </header>
   )
 }
 
